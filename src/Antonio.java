9532e34400861da75706708092c040d3c8b00be1
jorge.wellison <jorge.wellison@LAB2-DCX-32.dcx.ufpb.br>
2017-09-11 11:27:47 -0300

    Boa sorte HSUAHSUA

--- src/Antonio.java
+++ src/Antonio.java
@@ -1,5 +1,6 @@
 public class Antonio {
 	public static void main (String[] args){
 		System.out.println("Antonio");
+		System.out.println("Hernanes não salvará o SP, o rebaixamento já é uma realidade!");
 	}
 }
